c7c907615dd7325fe5e92ae5f5109874cfa169f9
Antti Pham <antti.pham@noreply.com>
2026-01-20 11:07:39 +0200

    Search for tag [foot: designated] to get only pedestrian roads

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
+import streamlit as st
 from streamlit_folium import st_folium
 
 import map
 
 if __name__ == "__main__":
     m = map.build_map()
-    st_data = st_folium(m, width=725)
+    st_data = st_folium(m, use_container_width=True)
--- src/map.py
+++ src/map.py
@@ -1,18 +1,21 @@
 import folium
 import osmnx as ox
 
+PLACE_NAME = "Roves"
+
 
 def build_map() -> folium.Map:
-    graph = ox.graph.graph_from_place("Roves", network_type="walk")
-    m = folium.Map(
-        location=ox.geocode("Roves"),
+    map = folium.Map(
+        location=ox.geocode(PLACE_NAME),
         tiles=None,
         zoom_start=13,
         attributionControl=False,
     )
 
-    folium.TileLayer(tiles='openstreetmap', name='OpenStreetMap').add_to(m)
+    # Fall back to OpenStreetMap when WMS layer is loading
+    folium.TileLayer(tiles="openstreetmap", name="OpenStreetMap").add_to(map)
 
+    # Add Seinäjoki satellite image WMS layer
     folium.WmsTileLayer(
         url="https://kartat.seinajoki.fi/teklaogcweb/wms.ashx",
         layers="Hybridi-ilmakuva",
@@ -21,8 +24,9 @@ def build_map() -> folium.Map:
         name="Ilmakuva",
         overlay=False,
         show=True,
-    ).add_to(m)
+    ).add_to(map)
 
+    # Add Seinäjoki topographic map WMS layer
     folium.WmsTileLayer(
         url="https://kartat.seinajoki.fi/teklaogcweb/wms.ashx",
         layers="KantakartanMaastotiedot",
@@ -31,11 +35,18 @@ def build_map() -> folium.Map:
         name="Maastotiedot",
         overlay=True,
         show=True,
-    ).add_to(m)
+    ).add_to(map)
 
+    # Add OSMnx graph data to the map
+    graph = ox.graph.graph_from_place(
+        PLACE_NAME,
+        custom_filter='["foot"~"designated|yes"]',
+    )
+    # ox.save_graphml(graph, "graph.graphml")
     nodes, edges = ox.graph_to_gdfs(graph)
-    folium.GeoJson(edges, name="Tiet").add_to(m)
+    folium.GeoJson(edges, name="Tiet").add_to(map)
 
-    folium.LayerControl().add_to(m)
+    # Add layer control to switch layers on and off
+    folium.LayerControl().add_to(map)
 
-    return m
+    return map
