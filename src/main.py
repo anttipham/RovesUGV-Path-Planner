39d5820d3c1ea6512fffe90437ae40e5f428d9b1
Antti Pham <antti.pham@noreply.com>
2026-01-20 15:40:42 +0200

    Add page title and display st_data for debug

--- src/main.py
+++ src/main.py
@@ -4,5 +4,8 @@ from streamlit_folium import st_folium
 import map
 
 if __name__ == "__main__":
+    st.set_page_config(page_title="UGV Roves", layout="wide")
     m = map.build_map()
     st_data = st_folium(m, use_container_width=True)
+    st.text("Map interaction data:")
+    st.write(st_data)
