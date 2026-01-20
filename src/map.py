f6ca4e0a6e9db70ec7c548f3c67f9f59059591e3
Antti Pham <antti.pham@noreply.com>
2026-01-20 15:41:28 +0200

    Initialize draw plugin with OSM data

--- src/map.py
+++ src/map.py
@@ -5,6 +5,25 @@ import osmnx as ox
 PLACE_NAME = "Roves"
 
 
+def create_editable_road_layer() -> folium.FeatureGroup:
+    # Add OSMnx graph data to draw plugin
+    editable_road_layer = folium.FeatureGroup(name="Tieverkko")
+    osm_graph = ox.graph.graph_from_place(
+        PLACE_NAME,
+        custom_filter='["foot"~"designated|yes"]',
+    )
+    nodes, edges = ox.graph_to_gdfs(osm_graph)
+    # Loop edges to add them to the map
+    for _, edge in edges.iterrows():
+        coords = [(y, x) for x, y in edge["geometry"].coords]
+        folium.PolyLine(
+            coords,
+            color="blue",
+        ).add_to(editable_road_layer)
+
+    return editable_road_layer
+
+
 def build_map() -> folium.Map:
     map = folium.Map(
         location=ox.geocode(PLACE_NAME),
@@ -38,19 +57,22 @@ def build_map() -> folium.Map:
         show=True,
     ).add_to(map)
 
-    # Add OSMnx graph data to the map
-    graph = ox.graph.graph_from_place(
-        PLACE_NAME,
-        custom_filter='["foot"~"designated|yes"]',
-    )
-    # ox.save_graphml(graph, "graph.graphml")
-    nodes, edges = ox.graph_to_gdfs(graph)
-    folium.GeoJson(edges, name="Tiet").add_to(map)
+    # Add editable road layer to the map
+    road_layer = create_editable_road_layer().add_to(map)
+    Draw(
+        export=True,
+        feature_group=road_layer,
+        draw_options={
+            "polyline": True,
+            "polygon": True,
+            "rectangle": False,
+            "circle": False,
+            "marker": True,
+            "circlemarker": False,
+        },
+    ).add_to(map)
 
     # Add layer control to switch layers on and off
     folium.LayerControl().add_to(map)
-
-    # Add drawing tools to the map
-    Draw(export=True).add_to(map)
 
     return map
