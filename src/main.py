ad065be259e28cb4e178df9f9d221b0a1112a4d4
Antti Pham <antti.pham@noreply.com>
2026-01-21 09:10:33 +0200

    Refactor to main function

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from streamlit_folium import st_folium
 
 import map
 
-css = """
+LEAFLET_STYLING = """
     <style>
     .leaflet-div-icon.leaflet-editing-icon {
         border-radius: 50%;
@@ -12,16 +12,21 @@ css = """
     </style>
 """
 
-if __name__ == "__main__":
+
+def main():
     # Initialize Streamlit app
     st.set_page_config(page_title="UGV Roves", layout="wide")
 
     # Load and build the map
     m = map.build_map()
     # Custom CSS to style the edit vertices as circles
-    m.get_root().header.add_child(folium.Element(css))
+    m.get_root().header.add_child(folium.Element(LEAFLET_STYLING))
 
     # Display the map in Streamlit and capture interaction data
     st_data = st_folium(m, use_container_width=True)
     st.text("Map interaction data:")
     st.write(st_data)
+
+
+if __name__ == "__main__":
+    main()
