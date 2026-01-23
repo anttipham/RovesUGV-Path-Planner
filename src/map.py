458aff45152017ce685c3e8ed1f405d06ca796c4
Antti Pham <antti.pham@noreply.com>
2026-01-23 15:59:13 +0200

    Fix bug: non-editable polylines. Add custom JS event to draw plugin.

--- src/map.py
+++ src/map.py
@@ -5,33 +5,43 @@ import osmnx as ox
 import config
 import road_graph
 
+on_create = folium.JsCode(
+    """
+    (e) => {
+        // Generate an id for the feature
+        var feature = (e.layer.feature = e.layer.feature || {})
+        feature.type = "Feature"
+        feature.properties = feature.properties || {}
+        feature.properties["id"] = Date.now();
+    }
+    """
+)
+
 
 def create_editable_road_layer() -> folium.FeatureGroup:
-    editable_road_layer = folium.FeatureGroup(name=config.EDITABLE_ROAD_LAYER_NAME)
+    editable_road_layer = folium.FeatureGroup(
+        name=config.EDITABLE_ROAD_LAYER_NAME,
+    )
 
     osm_graph = road_graph.create_road_graph()
-    # print(osm_graph.edges(keys=True, data=True))
     nodes, edges = ox.graph_to_gdfs(osm_graph)
+    # print(nodes.head())
     # print(edges.head())
 
     # Add edges and their metadata to the editable road layer
     for _, edge in edges.iterrows():
         # Make edge to geojson polyline and add to the layer
-        geojson_polyline = folium.GeoJson(
-            {
-                "type": "Feature",
-                "geometry": edge["geometry"].__geo_interface__,
-                "properties": edge.drop("geometry").to_dict(),
-            },
-            style_function=lambda x: {"color": "blue"},
-        )
-        geojson_polyline.add_to(editable_road_layer)
+        coords = [(y, x) for x, y in edge["geometry"].coords]
+        folium.PolyLine(
+            coords,
+            color="blue",
+        ).add_to(editable_road_layer)
 
     return editable_road_layer
 
 
 def build_map() -> folium.Map:
-    map = folium.Map(
+    m = folium.Map(
         location=ox.geocode(config.PLACE_NAME),
         tiles=None,
         zoom_start=13,
@@ -41,7 +51,7 @@ def build_map() -> folium.Map:
     # Fall back to OpenStreetMap when WMS layer is loading
     folium.TileLayer(
         tiles="openstreetmap", name=config.OPEN_STREET_MAP_LAYER_NAME
-    ).add_to(map)
+    ).add_to(m)
 
     # Add Seinäjoki satellite image WMS layer
     folium.WmsTileLayer(
@@ -52,7 +62,7 @@ def build_map() -> folium.Map:
         name=config.SEINAJOKI_SATELLITE_IMAGE_LAYER_NAME,
         overlay=False,
         show=True,
-    ).add_to(map)
+    ).add_to(m)
 
     # Add Seinäjoki topographic map WMS layer
     folium.WmsTileLayer(
@@ -63,10 +73,10 @@ def build_map() -> folium.Map:
         name=config.SEINAJOKI_TOPOGRAPHIC_IMAGE_LAYER_NAME,
         overlay=True,
         show=True,
-    ).add_to(map)
+    ).add_to(m)
 
     # Add editable road layer to the map
-    road_layer = create_editable_road_layer().add_to(map)
+    road_layer = create_editable_road_layer().add_to(m)
     Draw(
         export=True,
         filename="roves_ugv_map_data.geojson",
@@ -79,9 +89,16 @@ def build_map() -> folium.Map:
             "marker": True,
             "circlemarker": False,
         },
-    ).add_to(map)
+        on=handlers,
+    ).add_to(m)
+
+    # Add JavaScript events to Draw plugin
+    handlers = {
+        "draw:created": on_create,
+    }
+    m.on(**handlers)
 
     # Add layer control to switch layers on and off
-    folium.LayerControl().add_to(map)
+    folium.LayerControl().add_to(m)
 
-    return map
+    return m
