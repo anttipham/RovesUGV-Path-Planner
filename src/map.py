c67f2df31c30499eac96734fd4bdcfd3e385081f
Antti Pham <antti.pham@noreply.com>
2026-01-26 14:48:23 +0200

    Add buildings

--- src/map.py
+++ src/map.py
@@ -3,6 +3,7 @@ import folium
 import osmnx as ox
 
 import config
+import osm_gis
 
 LEAFLET_STYLING = """
     <style>
@@ -42,6 +43,33 @@ def add_tile_layers(m: folium.Map) -> None:
     ).add_to(m)
 
 
+def add_buildings(m: folium.Map) -> None:
+    gdf = osm_gis.get_building_geometries()
+    folium.GeoJson(
+        gdf,
+        name=config.BUILDING_LAYER_NAME,
+        style_function=lambda feature: {
+            "fillColor": "gray",
+            "color": "black",
+            "weight": 1,
+            "fillOpacity": 0.5,
+        },
+    ).add_to(m)
+
+
+def add_roads(m: folium.Map) -> None:
+    graph = osm_gis.create_road_graph()
+    folium.GeoJson(
+        ox.graph_to_gdfs(graph, nodes=False, fill_edge_geometry=True),
+        name=config.ROAD_LAYER_NAME,
+        style_function=lambda feature: {
+            "color": "blue",
+            "weight": 2,
+            "opacity": 0.7,
+        },
+    ).add_to(m)
+
+
 def build_map() -> folium.Map:
     m = folium.Map(
         location=ox.geocode(config.PLACE_NAME),
@@ -50,7 +78,11 @@ def build_map() -> folium.Map:
         attributionControl=False,
     )
 
+    # Construct map layers
     add_tile_layers(m)
+    add_buildings(m)
+    add_roads(m)
+
     # Add draw plugin to the map
     draw.add_draw_plugin(m)
 
--- src/osm_gis.py
+++ src/osm_gis.py
@@ -3,6 +3,15 @@ import osmnx as ox
 import config
 
 
+def get_building_geometries():
+    # Fetch building geometries from OSMnx
+    gdf = ox.features_from_place(
+        config.PLACE_NAME,
+        {"building": True},
+    )
+    return gdf
+
+
 def create_road_graph():
     # Add undirected OSMnx graph data to draw plugin
     graph = ox.graph.graph_from_place(
